d1d45d14c55053b43be304b50915958f5285fee8
Simon Cozens <simon@simon-cozens.org>
2021-08-19 11:06:43 +0100

    Check multiple languages at once

--- shaperglot/cli.py
+++ shaperglot/cli.py
@@ -10,27 +10,28 @@ def main(args=None):
     parser = argparse.ArgumentParser(description="Check a font file's language coverage")
     parser.add_argument('--verbose', '-v', action='count')
     parser.add_argument('font', metavar='FONT', help='the font file')
-    parser.add_argument('lang', metavar='LANG', help='an ISO639-3 language code')
+    parser.add_argument('lang', metavar='LANG', help='an ISO639-3 language code', nargs="+")
     options = parser.parse_args(args)
     checker = Checker(options.font)
     langs = Languages()
-    if options.lang not in langs:
-        print(f"Language '{options.lang}' not known")
-        sys.exit(1)
+    for lang in options.lang:
+        if lang not in langs:
+            print(f"Language '{options.lang}' not known")
+            continue
 
-    results = checker.check(langs[options.lang])
+        results = checker.check(langs[lang])
 
-    if results.is_success:
-        print(f"Font supports language '{options.lang}'")
-    else:
-        print(f"Font does not fully support language '{options.lang}'")
+        if results.is_success:
+            print(f"Font supports language '{lang}'")
+        else:
+            print(f"Font does not fully support language '{lang}'")
 
-    if options.verbose and options.verbose > 1:
-        for status, message in results:
-            print(f" * {status.value}: {message}")
-    elif options.verbose or not results.is_success:
-        for message in results.fails:
-            print(f" * {message}")
+        if options.verbose and options.verbose > 1:
+            for status, message in results:
+                print(f" * {status.value}: {message}")
+        elif options.verbose or not results.is_success:
+            for message in results.fails:
+                print(f" * {message}")
 
 if __name__ == '__main__':  # pragma: no cover
     main()
