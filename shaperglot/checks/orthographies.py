6a8d204dfdbeb1b0e50820b405b8b267608449b9
Denis Moyogo Jacquerye <moyogo@gmail.com>
2023-06-05 15:09:24 +0200

    orthographies: warn on missing auxiliary

--- shaperglot/checks/orthographies.py
+++ shaperglot/checks/orthographies.py
@@ -28,10 +28,10 @@ class OrthographiesCheck(ShaperglotCheck):
         marks = exemplar_chars.get("marks", "").replace("◌", "").split() or []
         bases = parse_bases(exemplar_chars.get("base", ""))
         aux = parse_bases(exemplar_chars.get("auxiliary", ""))
-        bases = bases + aux
         self.all_glyphs = marks + bases
         self.marks = set(marks)
         self.bases = set(bases) - self.marks
+        self.aux = set(aux) - self.marks
 
     def should_skip(self, checker):
         return False
@@ -63,7 +63,7 @@ class OrthographiesCheck(ShaperglotCheck):
                 message="All base glyphs were present in the font",
             )
 
-        if not self.marks:
+        if not self.marks and not self.aux:
             return
 
         missing = sorted([x for x in self.marks if not can_shape(x, checker)])
@@ -79,4 +79,21 @@ class OrthographiesCheck(ShaperglotCheck):
             checker.results.okay(
                 check_name="orthographies",
                 message="All mark glyphs were present in the font",
+            )
+
+        if not self.aux:
+            return
+
+        missing = sorted([x for x in self.bases if not can_shape(x, checker)])
+        if missing:
+            checker.results.warn(
+                check_name="orthographies",
+                result_code="aux-missing",
+                message=f"Some auxiliary glyphs were missing: {', '.join(missing)}",
+                context={"glyphs": missing},
+            )
+        else:
+            checker.results.okay(
+                check_name="orthographies",
+                message="All auxiliary glyphs were present in the font",
             )
